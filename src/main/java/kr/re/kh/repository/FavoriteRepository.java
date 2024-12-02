e7346f7b7dc650e5d040c28fcbe7d8e6d4956776
Jonghee Park <159970440+JongheeGit@users.noreply.github.com>
2024-12-02 09:12:39 +0900

    a

--- src/main/java/kr/re/kh/repository/FavoriteRepository.java
+++ src/main/java/kr/re/kh/repository/FavoriteRepository.java
@@ -2,9 +2,11 @@ package kr.re.kh.repository;
 
 import kr.re.kh.entity.Favorite;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
     List<Favorite> findByFolderId(Long folderId);
 }
--- src/main/java/kr/re/kh/repository/FolderRepository.java
+++ src/main/java/kr/re/kh/repository/FolderRepository.java
@@ -3,6 +3,8 @@ package kr.re.kh.repository;
 
 import kr.re.kh.entity.Folder;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FolderRepository extends JpaRepository<Folder, Long> {
 }
